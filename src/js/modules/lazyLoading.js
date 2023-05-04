a5d087e6c4a8c49ad204bbacdb894753714229ac
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-05-04 23:20:11 +0600

    added cheking support browser functional lazy loading

--- src/js/modules/lazyLoading.js
+++ src/js/modules/lazyLoading.js
@@ -1,4 +1,28 @@
 export const lazyLoading = () => {
-	const lazyImages = document.querySelector('.img[data-src]');
+
+	// if ('loading' in HTMLImageElement.prototype) {
+	// 	// supported in browser
+	// } else {
+	// };
+
+	const lazyImages = document.querySelectorAll('img[data-src]');
+	const loadMapBlock = document.querySelector('._loadmap');
+	const windowHeight = document.documentElement.clientHeight;
+
+	console.log(lazyImages);
+
+	let lazyImagesPosition = [];
+
+	if (lazyImages.length > 0) {
+		lazyImages.forEach(img => {
+			console.log(img);
+			if (img.dataset.src) {
+				lazyImagesPosition.push(img.getBoundingClientRect().top)
+			};
+		})
+	};
+
+	console.log(lazyImagesPosition);
+
 
 }
